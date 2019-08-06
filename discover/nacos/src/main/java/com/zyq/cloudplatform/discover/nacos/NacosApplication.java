0d4dc81f42ec12da7043c97ca20ab7cb274c9658
张运奇 <zhangyq@gitlab.com>
2019-08-06 18:02:53 +0800

    阿里巴巴微服务注册中心Nacos

---
+++ discover/nacos/src/main/java/com/zyq/cloudplatform/discover/nacos/NacosApplication.java
@@ -0,0 +1,13 @@
+package com.zyq.cloudplatform.discover.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosApplication.class, args);
+    }
+
+}
