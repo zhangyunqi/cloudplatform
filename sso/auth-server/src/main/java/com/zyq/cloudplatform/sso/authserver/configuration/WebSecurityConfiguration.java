cf760f65ef93ad8d0ac4e576bd921e0301af8033
zhangyunqi <1196385490@qq.com>
2019-08-21 00:18:34 +0800

    Oauth2.0

--- sso/auth-server/src/main/java/com/zyq/cloudplatform/sso/authserver/configuration/WebSecurityConfiguration.java
+++ sso/auth-server/src/main/java/com/zyq/cloudplatform/sso/authserver/configuration/WebSecurityConfiguration.java
@@ -3,19 +3,25 @@ package com.zyq.cloudplatform.sso.authserver.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * web安全配置
+ *
  * @author zhangyunqi
  * @date 2019/08/09
  */
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
     /**
      * 密码模式必须
      * 在UsernamePasswordAuthenticationFilter的attemptAuthentication()方法中，调用AuthenticationManager进行认证
+     *
      * @return
      * @throws Exception
      */
@@ -25,12 +31,35 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         return super.authenticationManager();
     }
 
+    @Override
+    @Bean
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
+    }
+
+
     /**
      * spring 5以后，密码必须加密
+     *
      * @return
      */
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    /**
+     * 配置所有请求的安全验证
+     *
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().fullyAuthenticated();
+    }
+
+
+
+
 }
