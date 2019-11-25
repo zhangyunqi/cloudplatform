f32ebaa5ed45abf7465290eea1eff761ded2f9c6
zhangyunqi <1196385490@qq.com>
2019-11-25 19:48:06 +0800

    1、cloud-gatway加入路由 2、ip注冊

---
+++ gateway/cloud-gateway/src/main/java/com/zyq/cloudplatform/gateway/cloudgateway/CloudGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.zyq.cloudplatform.gateway.cloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关
+ * @author zhangyunqi
+ * @date 2019/11/20 12:25
+ */
+@SpringBootApplication
+public class CloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGatewayApplication.class, args);
+    }
+
+}
---
+++ gateway/cloud-gateway/src/test/java/com/zyq/cloudplatform/gateway/cloudgateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyq.cloudplatform.gateway.cloudgateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
